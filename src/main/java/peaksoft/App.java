d21986c931b5d34c9b245144572b9259b689a31f
beckbolot <bekoxna@gmail.com>
2021-07-14 09:30:28 +0300

    Peaksoft sprin homework1

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -7,8 +7,27 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        System.out.println("Comparing objects equality with bean1 and bean2");
+        System.out.println(bean1==bean2);
+        System.out.println("Comparing hashcodes of bean1 and bean2");
+        System.out.println("bean1 hashcode->" + bean1.hashCode());
+        System.out.println("bean2 hashcode->" + bean2.hashCode());
+
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Comapring objects equality with cat1 and cat2");
+        System.out.println(cat1==cat2);
+        System.out.println("Comparing hashcodes of cat1 and cat2");
+        System.out.println("cat1 hashcode->" + cat1.hashCode());
+        System.out.println("cat2 hashcode->" + cat2.hashCode());
+
     }
 }
--- src/main/java/peaksoft/AppConfig.java
+++ src/main/java/peaksoft/AppConfig.java
@@ -2,6 +2,7 @@ package peaksoft;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +11,11 @@ public class AppConfig {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat getCat() {
+        return new Cat();
     }
 }
